870efcb0d26cbd6aaa1d078c9cab9c8baac9ecc8
Jurassickk <cuellardiego310@gmail.com>
2025-08-27 10:40:07 -0500

    feat(frontend): add dashboard component vehicule

--- Web-Admin/src/app/layout.tsx
+++ Web-Admin/src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "Landing Page",
+  title: "Dashboard UrbanTracker",
   description: "Sistema de gestión de transporte y logística",
   generator: "v0.app",
   icons: {
